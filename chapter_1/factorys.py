03f029760294408f51af1a84db346da5cad0ddaf
cissnei <cissnei@naver.com>
2017-04-05 00:39:26 +0900

    test

--- chapter_1/factorys.py
+++ chapter_1/factorys.py
@@ -10,4 +10,31 @@ df=df.replace({'  ?':np.nan})
 df=df.replace({'   ?':np.nan})
 df=df.replace({'    ?':np.nan})
 df=df.replace({'     ?':np.nan})
+df=df.replace({'      ?':np.nan})
 df=df.fillna(-1)
+
+adindices = df[df.columns[-1]] =='ad.'
+df.loc[adindices, df.columns[-1]]=1
+nonadindices = df[df.columns[-1]] =='nonad.'
+df.loc[nonadindices, df.columns[-1]]=0
+df[df.columns[-1]] = df[df.columns[-1]].astype(float)
+df.apply(lambda x: pd.to_numeric(x))
+
+dataset = df.values[:,:]
+
+np.random.shuffle(dataset)
+data= dataset[:,:-1]
+labels = dataset[:,:-1].astype(float)
+ntrainrows=int(len(data)*.8)
+
+train = data[:ntrainrows,:]
+trainlabels = labels[:ntrainrows]
+test = data[ntrainrows:,:]
+testlabels = labels[ntrainrows:]
+
+from sklearn.svm import SVC
+clf = SVC(gamma=0.001, C=100.)
+clf.fit(train, trainlabels)
+
+score = clf.score(test, testlabels)
+print 'score:',score
